e8dd63b8f4f18a7f0bfbe36c3adba96d6441222e
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-12-14 23:08:58 -0600

    changed front-end js to have url: pathName so it will work when deployed.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,22 +4,19 @@ $(document).ready(function () {
     const newBurger = {
       burgerName: $("#burger-name").val().trim(),
     };
-    $.ajax("/api/burgers/", {
-      type: "POST",
-      data: newBurger,
-    }).then(() => {
+    $.ajax({ url: "/api/burgers/", type: "POST", data: newBurger }).then(() => {
       location.reload();
     });
   });
   $(".delete-btn").click(function () {
     const id = $(this).data("id");
-    $.ajax("/api/burgers/" + id, { type: "DELETE" }).then(() => {
+    $.ajax({ url: "/api/burgers/" + id, type: "DELETE" }).then(() => {
       location.reload();
     });
   });
   $(".eat-btn").click(function () {
     const id = $(this).data("id");
-    $.ajax("/api/burgers/" + id, { type: "PUT" }).then(() => {
+    $.ajax({ url: "/api/burgers/" + id, type: "PUT" }).then(() => {
       location.reload();
     });
   });
