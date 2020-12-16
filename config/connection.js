5a96fb4a925098c00c8fa9f910334afcca51a872
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-12-15 21:36:08 -0600

    I actually looked at the PDF that I thought was a broken file. Heroku deployment incoming (hopefully).

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db",
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
