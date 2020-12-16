ad52eaee309c3ce45847712125879ff921e9afdf
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-12-15 21:54:00 -0600

    Saved changes required for uploading to heroku in my schema.

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(127) NOT NULL,
-  eaten BOOLEAN DEFAULT false
+  eaten BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP NOT NULL,
+  PRIMARY KEY(id)
 );
